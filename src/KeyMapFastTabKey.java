a2e71cf2158736149a0030cbb6c2dfdd6715e669
zhikaixin <1147271475@qq.com>
2019-03-08 23:36:16 +0800

    Initial commit test

---
+++ src/KeyMapFastTabKey.java
@@ -0,0 +1,31 @@
+/**
+ * @author zhikaixin
+ * @date 2019/3/7 - 11:38
+ */
+public class KeyMapFastTabKey {
+    /*ctrl+d是复制
+     * alr+r是执行
+     * alt+/是代码提示????????很烦
+     * alt+shift+down/ctrl+shift+down是代码本体向下移动一行
+     * ctrl+alt+l是格式化
+     * ctrl+shift+enter是把光标定位在下一行
+     * ctrl+r是查找
+     * ctrl+j是给出代码的简易提示
+     * */
+    public static void main(String[] args) {
+
+        System.out.println("----------");
+        System.out.println("----------");
+        KeyMapFastTabKey key = new KeyMapFastTabKey();
+    }
+    private String name;
+    private  int age;
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        ArrayList list=new ArrayList();
+
+    }
+}
